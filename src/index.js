fba11cc446d8ef674dc2c050273bce14f8ecce7d
1sabraman <97954548+1sabraman@users.noreply.github.com>
2022-07-15 12:10:09 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-/**
-  This CodeSandbox has been automatically generated using
-  `codesandboxer`. If you're curious how that happened, you can
-  check out our docs here: https://github.com/codesandbox/codesandboxer
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-  If you experience any struggles with this sandbox, please raise an issue
-  on github. :)
-*/
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./example";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-// import ReactDOM from "react-dom";
-// import App from 'App';
-
-const container = document.getElementById("root");
-
-// Create a root.
-const root = ReactDOM.createRoot(container);
-
-// Initial render
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
